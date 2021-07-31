128e3300954b71bb54318f4f8c79c8035cd8faac
vpc <cuevasvp@gmail.com>
2021-07-31 13:44:36 +0800

    new

---
+++ sql/result_set_demo.sql
@@ -0,0 +1,20 @@
+﻿--#SET TERMINATOR #
+create procedure result_set_demo ()
+      language sql
+      dynamic result sets 2
+begin
+   declare c1 cursor for
+      select *
+         from department
+         for read only;
+   declare c2 cursor for
+      select *
+         from employee
+         for read only;
+   open c1;
+   open c2;
+   return;
+end;
+
+call vpcrzkh1.result_set_demo();
+
