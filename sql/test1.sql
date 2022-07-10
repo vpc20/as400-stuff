0e87499b1a96126d10ac1aab10e2ba3517cc07a1
vpc <cuevasvp@gmail.com>
2022-07-10 11:15:59 +0800

    new

---
+++ sql/test1.sql
@@ -0,0 +1,18 @@
+﻿create or replace table vpcrzkh1.agthist (polnum integer not null, 
+                                agtnum integer not null,
+                                strdte integer not null);
+                                
+insert into  vpcrzkh1.agthist values(111, 777, 20220101);                          
+insert into  vpcrzkh1.agthist values(111, 888, 20220201);                          
+insert into  vpcrzkh1.agthist values(111, 999, 20220301);                          
+insert into  vpcrzkh1.agthist values(222, 777, 20220401);                          
+insert into  vpcrzkh1.agthist values(222, 888, 20220501);                          
+insert into  vpcrzkh1.agthist values(222, 999, 20220601);     
+
+select * from  vpcrzkh1.agthist;
+
+select polnum, agtnum, strdte,
+   ROW_NUMBER() OVER (PARTITION BY polnum
+                      ORDER BY transaction_time DESC) 
+from  vpcrzkh1.agthist; 
+
