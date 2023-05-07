4b89f9e5acba388f8e923cdb15073491d9e0d473
vpc20 <cuevasvp@yahoo.com>
2023-05-07 15:20:19 +0800

    revision

--- sql/get_primary_key.sql
+++ sql/get_primary_key.sql
@@ -12,3 +12,11 @@ select table_cat, ordinal_position, column_name, asc_or_desc
          and table_name = 'PROCS'
          and ordinal_position is not null
    order by ordinal_position;
+
+
+select table_cat, ordinal_position, column_name, asc_or_desc
+   from sysibm.sqlstatistics
+   where table_schem = 'VPCRZKH1'
+         and table_name = 'MSGPF'
+         and ordinal_position is not null
+   order by ordinal_position;   
