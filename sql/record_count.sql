c9235e44fb8400ebbff871490e2710ea0255ca18
vpc20 <cuevasvp@yahoo.com>
2023-09-08 20:11:51 +0800

    new function

---
+++ sql/record_count.sql
@@ -0,0 +1,27 @@
+﻿set current schema vpcrzkh1;
+
+drop function vpcrzkh1.record_count;
+
+create function vpcrzkh1.record_count (
+      in_filename char(10), in_libname char(10)
+  )
+  returns bigint
+  language sql
+  --
+  begin
+  --
+    declare rec_count bigint;
+    declare c1 cursor for
+      select number_rows
+        from qsys2.syspartitionstat
+        where table_schema = upper(in_libname)
+              and table_name = upper(in_filename);
+    open c1;
+    fetch c1
+      into rec_count;
+    --
+    return rec_count;
+    --
+  end;
+  
+select vpcrzkh1.record_count('msgpf', 'vpcrzkh1') FROM sysibm.sysdummy1;
