76972ec38364ef1c4fe1f652f69d81eee19254e1
vpc20 <cuevasvp@yahoo.com>
2023-10-30 18:58:25 +0800

    new sql

---
+++ sql/string_last_occurence.sql
@@ -0,0 +1,15 @@
+﻿set current schema vpcrkzh1;
+
+select right('first.last@yahoo.com', length('first.last@yahoo.com') - locate_in_string('first.last@yahoo.com', '.', -1, 1))
+  from sysibm.sysdummy1;
+
+-- last occurence of .
+select substr('first.last@yahoo.com', locate_in_string('first.last@yahoo.com', '.', -1, 1))
+  from sysibm.sysdummy1;
+
+-- second to the last occurence of .
+select substr('first.last@yahoo.com.ph', locate_in_string('first.last@yahoo.com.ph', '.', -1, 2))
+  from sysibm.sysdummy1;
+  
+  
+  
