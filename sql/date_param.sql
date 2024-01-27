c41aeb5cd9e6775ae9d8ed85e4b9cbb929494a24
vpc20 <cuevasvp@yahoo.com>
2024-01-27 17:28:29 +0800

    new sql

---
+++ sql/date_param.sql
@@ -0,0 +1,20 @@
+﻿set current schema vpcrzkh1;
+
+CREATE PROCEDURE vpcrzkh1.date_param (IN in_date DATE)
+    LANGUAGE sql
+    DYNAMIC RESULT SETS 1
+BEGIN
+  DECLARE c1 CURSOR FOR
+    SELECT in_date
+      FROM sysibm.sysdummy1;
+  OPEN c1;
+  RETURN;
+END;
+
+call vpcrzkh1.date_param('2024-12-31');
+
+-- Data type mismatch
+-- call vpcrzkh1.date_param('12/31/2024');
+-- call vpcrzkh1.date_param('31.12.2024');
+
+-- run from ACS Run SQL scripts to get interactive results
