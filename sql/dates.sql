447b4a6ce685c2657ad846d039cae1264996e5d6
vpc20 <cuevasvp@yahoo.com>
2024-01-28 06:36:53 +0800

    add comments

--- sql/dates.sql
+++ sql/dates.sql
@@ -22,19 +22,10 @@ select left(19881225,4) || '-' || substr(char(19881225), 5, 2) ||
          from sysibm.sysdummy1;
 
 -- date arithmetic   
-select days(current date) - days(date('2023-08-01'))  -- days difference
+select days(current date) - days(date('2023-08-01'))  -- diff in days
   from sysibm.sysdummy1;
 
-select date(days(current date) + 1) -- add 1 day
-   from sysibm.sysdummy1;
-
-select date(days(current date) - 1) -- subtract 1 day
-   from sysibm.sysdummy1;
-
-select add_months(current date, 1)  -- add months (day will be last day is input date is last day of month)
-   from sysibm.sysdummy1;
-
-select months_between(current date, '2021-06-01')
+select months_between(current date, '2021-06-01')  -- diff in months
    from sysibm.sysdummy1;
 
 select year(current date - date('1943-05-23')) -- diff in years
@@ -43,16 +34,16 @@ from sysibm.sysdummy1;
 select year(current date - date('2023-12-01')) -- diff in years
 from sysibm.sysdummy1;
 
-select current date + 1 year from sysibm.sysdummy1;
-select current date + 1 month from sysibm.sysdummy1;
-select current date + 1 day from sysibm.sysdummy1;
+select current date + 1 year from sysibm.sysdummy1;   -- add 1 year
+select current date + 1 month from sysibm.sysdummy1;  -- add 1 month (does not change day of month)
+select current date + 1 day from sysibm.sysdummy1;    -- add 1 day
 
-select current date - 1 year from sysibm.sysdummy1;
-select current date - 1 month from sysibm.sysdummy1; -- add 1 month (does not change day of month)
-select current date - 1 day from sysibm.sysdummy1;
-
-select ADD_MONTHS('2000-2-28', 1) from sysibm.sysdummy1; -- add 1 month
+select ADD_MONTHS('2000-2-28', 1) from sysibm.sysdummy1; -- add 1 month (last day is returned if input is last day of month)
 select ADD_MONTHS('2000-2-29', 1) from sysibm.sysdummy1; -- add 1 month but returns last day of month
+
+select current date - 1 year from sysibm.sysdummy1;     -- subtract 1 year
+select current date - 1 month from sysibm.sysdummy1;    -- subtract 1 month
+select current date - 1 day from sysibm.sysdummy1;      -- subtract 1 day
 
 -- get system date/time
 select curdate() from sysibm.sysdummy1;
