497f286ed1cef9b68fa9da9961c68146e240c1e9
vpc20 <cuevasvp@gmail.com>
2025-11-02 07:45:25 +0800

    new

---
+++ sql/insert_empprojact.sql
@@ -0,0 +1,93 @@
+﻿set current schema vpcrzkh1;
+
+select * from vpcrzkh1.empprojact;
+
+truncate vpcrzkh1.empprojact;
+
+insert into vpcrzkh1.empprojact values('000010', 'AD3100', 10 ,  .50, '1982-01-01', '1982-07-01');
+insert into vpcrzkh1.empprojact values('000070', 'AD3110', 10 , 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000230', 'AD3111', 60 , 1.00, '1982-01-01', '1982-03-15');
+insert into vpcrzkh1.empprojact values('000230', 'AD3111', 60 ,  .50, '1982-03-15', '1982-04-15');
+insert into vpcrzkh1.empprojact values('000230', 'AD3111', 70 ,  .50, '1982-03-15', '1982-10-15');
+insert into vpcrzkh1.empprojact values('000230', 'AD3111', 80 ,  .50, '1982-04-15', '1982-10-15');
+insert into vpcrzkh1.empprojact values('000230', 'AD3111', 180,  .50, '1982-10-15', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000240', 'AD3111', 70 , 1.00, '1982-02-15', '1982-09-15');
+insert into vpcrzkh1.empprojact values('000240', 'AD3111', 80 , 1.00, '1982-09-15', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 60 , 1.00, '1982-01-01', '1982-02-01');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 60 ,  .50, '1982-02-01', '1982-03-15');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 60 , 1.00, '1983-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 70 ,  .50, '1982-02-01', '1982-03-15');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 70 , 1.00, '1982-03-15', '1982-08-15');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 70 ,  .25, '1982-08-15', '1982-10-15');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 80 ,  .25, '1982-08-15', '1982-10-15');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 80 ,  .50, '1982-10-15', '1982-12-01');
+insert into vpcrzkh1.empprojact values('000250', 'AD3112', 180,  .50, '1982-08-15', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000260', 'AD3113', 70 ,  .50, '1982-06-15', '1982-07-01');
+insert into vpcrzkh1.empprojact values('000260', 'AD3113', 70 , 1.00, '1982-07-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000260', 'AD3113', 80 , 1.00, '1982-01-01', '1982-03-01');
+insert into vpcrzkh1.empprojact values('000260', 'AD3113', 80 ,  .50, '1982-03-01', '1982-04-15');
+insert into vpcrzkh1.empprojact values('000260', 'AD3113', 180,  .50, '1982-03-01', '1982-04-15');
+insert into vpcrzkh1.empprojact values('000260', 'AD3113', 180, 1.00, '1982-04-15', '1982-06-01');
+insert into vpcrzkh1.empprojact values('000260', 'AD3113', 180, 1.00, '1982-06-01', '1982-07-01');
+insert into vpcrzkh1.empprojact values('000270', 'AD3113', 60 ,  .50, '1982-03-01', '1982-04-01');
+insert into vpcrzkh1.empprojact values('000270', 'AD3113', 60 , 1.00, '1982-04-01', '1982-09-01');
+insert into vpcrzkh1.empprojact values('000270', 'AD3113', 60 ,  .25, '1982-09-01', '1982-10-15');
+insert into vpcrzkh1.empprojact values('000270', 'AD3113', 70 ,  .75, '1982-09-01', '1982-10-15');
+insert into vpcrzkh1.empprojact values('000270', 'AD3113', 70 , 1.00, '1982-10-15', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000270', 'AD3113', 80 , 1.00, '1982-01-01', '1982-03-01');
+insert into vpcrzkh1.empprojact values('000270', 'AD3113', 80 ,  .50, '1982-03-01', '1982-04-01');
+insert into vpcrzkh1.empprojact values('000030', 'IF1000', 10 ,  .50, '1982-06-01', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000130', 'IF1000', 90 , 1.00, '1982-10-01', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000130', 'IF1000', 10 , 0.50, '1982-10-01', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000140', 'IF1000', 90 ,  .50, '1982-10-01', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000030', 'IF2000', 10 ,  .50, '1982-01-01', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000140', 'IF2000', 100, 1.00, '1982-01-01', '1982-03-01');
+insert into vpcrzkh1.empprojact values('000140', 'IF2000', 100,  .50, '1982-03-01', '1982-07-01');
+insert into vpcrzkh1.empprojact values('000140', 'IF2000', 110,  .50, '1982-03-01', '1982-07-01');
+insert into vpcrzkh1.empprojact values('000140', 'IF2000', 110,  .50, '1982-10-01', '1983-01-01');
+insert into vpcrzkh1.empprojact values('000010', 'MA2100', 10 ,  .50, '1982-01-01', '1982-11-01');
+insert into vpcrzkh1.empprojact values('000110', 'MA2100', 20 , 1.00, '1982-01-01', '1983-03-01');
+insert into vpcrzkh1.empprojact values('000010', 'MA2110', 10 , 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000200', 'MA2111', 50 , 1.00, '1982-01-01', '1982-06-15');
+insert into vpcrzkh1.empprojact values('000200', 'MA2111', 60 , 1.00, '1982-06-15', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000220', 'MA2111', 40 , 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000150', 'MA2112', 60 , 1.00, '1982-01-01', '1982-07-15');
+insert into vpcrzkh1.empprojact values('000150', 'MA2112', 180, 1.00, '1982-07-15', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000170', 'MA2112', 60 , 1.00, '1982-01-01', '1983-06-01');
+insert into vpcrzkh1.empprojact values('000170', 'MA2112', 70 , 1.00, '1982-06-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000190', 'MA2112', 70 , 1.00, '1982-01-01', '1982-10-01');
+insert into vpcrzkh1.empprojact values('000190', 'MA2112', 80 , 1.00, '1982-10-01', '1983-10-01');
+insert into vpcrzkh1.empprojact values('000160', 'MA2113', 60 , 1.00, '1982-07-15', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000170', 'MA2113', 80 , 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000180', 'MA2113', 70 , 1.00, '1982-04-01', '1982-06-15');
+insert into vpcrzkh1.empprojact values('000210', 'MA2113', 80 ,  .50, '1982-10-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000210', 'MA2113', 180,  .50, '1982-10-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000050', 'OP1000', 10 ,  .25, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000090', 'OP1010', 10 , 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000280', 'OP1010', 130, 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000290', 'OP1010', 130, 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000300', 'OP1010', 130, 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000310', 'OP1010', 130, 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000050', 'OP2010', 10 ,  .75, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000100', 'OP2010', 10 , 1.00, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000320', 'OP2011', 140,  .75, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000320', 'OP2011', 150,  .25, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000330', 'OP2012', 140,  .25, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000330', 'OP2012', 160,  .75, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000340', 'OP2013', 140,  .50, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000340', 'OP2013', 170,  .50, '1982-01-01', '1983-02-01');
+insert into vpcrzkh1.empprojact values('000020', 'PL2100', 30 , 1.00, '1982-01-01', '1982-09-15');
+
+select * from vpcrzkh1.empprojact;
+
+
+SELECT EMPNO, PROJNO
+    FROM vpcrzkh1.empprojact
+    group by EMPNO, PROJNO
+    order by EMPNO, PROJNO;
+
+
+SELECT EMPNO, count(*)
+    FROM vpcrzkh1.empprojact
+    group by EMPNO
+    order by EMPNO;
