9b2bed7d08e72b27304d21c57e7b7381ec616705
vpc20 <cuevasvp@gmail.com>
2025-11-06 16:58:40 +0800

    new examples

---
+++ sql/create_table_ex1.sql
@@ -0,0 +1,18 @@
+﻿SET CURRENT schema vpcrzkh1;
+
+CREATE TABLE inventory (
+      partno SMALLINT NOT NULL,
+      descr VARCHAR(24),
+      qonhand INT,
+      PRIMARY KEY (partno)
+    );
+    
+
+CREATE TABLE employee2 LIKE employee;
+
+CREATE TABLE employee3 AS
+      (SELECT empno, lastname, job, workdept
+          FROM employee
+          WHERE workdept = 'D11')
+      WITH DATA;
+      
