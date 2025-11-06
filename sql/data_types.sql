3fac712c7d16ff67f78165cb99c5bfd0eca3d851
vpc20 <cuevasvp@gmail.com>
2025-11-06 16:36:06 +0800

    add comment

--- sql/data_types.sql
+++ sql/data_types.sql
@@ -2,8 +2,8 @@
 -- CHAR
 -- VARCHAR
 
--- NUMERIC
--- DECIMAL
+-- NUMERIC     zoned decimal
+-- DECIMAL     packed decimal
 -- SMALLINT   -32 768 to +32 767
 -- INTEGER    -2 147 483 648 to +2 147 483 647 
 -- BIGINT     -9 223 372 036 854 775 808 to +9 223 372 036 854 775 807
