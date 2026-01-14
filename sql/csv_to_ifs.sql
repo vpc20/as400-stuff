585009a136d57408fd42a9a257aad5da12c3089d
vpc20 <cuevasvp@gmail.com>
2026-01-14 17:30:04 +0800

    revision

---
+++ sql/csv_to_ifs.sql
@@ -0,0 +1,18 @@
+﻿--
+--  category:  SYSTOOLS
+--  description:  Generate a spreadsheet, from a Db2 file
+--  minvrm: v7r4m0
+--
+-- https://www.ibm.com/docs/en/i/7.5?topic=services-generate-spreadsheet-scalar-function
+--
+create table vpcrzkh1.seccfg as
+      (select * from qsys2.security_info)
+      with data;
+
+VALUES SYSTOOLS.GENERATE_SPREADSHEET(
+  PATH_NAME         => '/home/vpcrzkh/sec_info',
+  LIBRARY_NAME      => 'VPCRZKH1', 
+  FILE_NAME         => 'SECCFG',     
+  SPREADSHEET_TYPE  => 'csv'    
+);
+
---
+++ sql/json_to_ifs.sql
@@ -0,0 +1,35 @@
+﻿BEGIN
+  -- 1. Declare a variable to hold the generated JSON (up to 2GB)
+  DECLARE my_json_output CLOB(2G) CCSID 1208;
+
+  -- 2. Execute your query and store the result in the variable
+  SET my_json_output = (
+    WITH json_rows (j) AS (
+      SELECT JSON_OBJECT(
+          KEY 'EMPNO' VALUE empno, KEY 'FIRSTNME' VALUE firstnme,
+          KEY 'MIDINIT' VALUE midinit, KEY 'LASTNAME' VALUE lastname,
+          KEY 'WORKDEPT' VALUE workdept, KEY 'PHONENO' VALUE phoneno,
+          KEY 'HIREDATE' VALUE hiredate, KEY 'JOB' VALUE job,
+          KEY 'EDLEVEL' VALUE edlevel, KEY 'SEX' VALUE sex,
+          KEY 'BIRTHDATE' VALUE birthdate, KEY 'SALARY' VALUE salary,
+          KEY 'BONUS' VALUE bonus, KEY 'COMM' VALUE comm
+          ABSENT ON NULL RETURNING CLOB(2G) CCSID 1208 FORMAT JSON
+        )
+        FROM vpcrzkh1.employee
+        ORDER BY lastname)
+    SELECT JSON_OBJECT(
+        KEY 'VPCRZKH1.EMPLOYEE' VALUE JSON_ARRAYAGG(
+          j FORMAT JSON
+        ) ABSENT ON NULL RETURNING CLOB(2G) CCSID 1208 FORMAT JSON
+      )
+      FROM json_rows
+  );
+
+  -- 3. Write the variable contents to an IFS file
+  CALL QSYS2.IFS_WRITE_UTF8(
+    PATH_NAME => '/home/vpcrzkh/employee_data.json',
+    LINE => my_json_output,
+    OVERWRITE => 'REPLACE',
+    END_OF_LINE => 'NONE'
+  );
+END;
