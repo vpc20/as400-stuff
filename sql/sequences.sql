2437fed6f253af445e3d0106fecb12cd21071616
vpc20 <cuevasvp@gmail.com>
2026-01-18 19:29:47 +0800

    new sql

---
+++ sql/sequences.sql
@@ -0,0 +1,21 @@
+﻿SET CURRENT schema vpcrzkh1;
+
+CREATE SEQUENCE order_seq
+  START WITH 500
+  INCREMENT BY 1
+  MAXVALUE 1000
+  CYCLE
+  CACHE 24;
+
+
+CREATE TABLE orders_with_seq (
+      orderno SMALLINT NOT NULL,
+      custno SMALLINT
+    );
+
+
+INSERT INTO orders_with_seq (orderno, custno)
+  VALUES (NEXT VALUE FOR order_seq, 12);
+  
+select * from orders_with_seq;
+
