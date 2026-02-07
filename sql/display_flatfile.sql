90b896d03d1effcc27f6e9f1f8a1369c162e41f7
vpc20 <cuevasvp@gmail.com>
2026-02-07 13:20:58 +0800

    new sql

---
+++ sql/display_flatfile.sql
@@ -0,0 +1,8 @@
+﻿set current schema vpcrzkh1;
+
+-- display the packed-decimal from the flat file
+SELECT                                                        
+    INTERPRET(BINARY(SUBSTR(flatfld, 5, 3)) AS decimal(5,2))  
+FROM flatfile ;       
+
+                                                                                      
