dbae70e8380e01e96b460f149c1dbece1aa41907
vpc20 <cuevasvp@gmail.com>
2026-02-07 17:13:44 +0800

    various revisions

--- sql/display_flatfile.sql
+++ sql/display_flatfile.sql
@@ -1,8 +1,15 @@
 ﻿set current schema vpcrzkh1;
 
 -- display the packed-decimal from the flat file
-SELECT                                                        
-    INTERPRET(BINARY(SUBSTR(flatfld, 5, 3)) AS decimal(5,2))  
-FROM flatfile ;       
+SELECT SUBSTR(flatfld, 1, 4) AS itemcode, 
+       SUBSTR(flatfld, 5, 3) AS rawpackedpos,
+       INTERPRET(BINARY(SUBSTR(flatfld, 5, 3)) AS DECIMAL(5, 2)) valpackedpos,
+       SUBSTR(flatfld, 8, 3) AS rawpackedneg,
+       INTERPRET(BINARY(SUBSTR(flatfld, 8, 3)) AS DECIMAL(5, 2)) valpackedneg,
+       SUBSTR(flatfld, 11, 5) AS rawzonedpos,
+       INTERPRET(BINARY(SUBSTR(flatfld, 11, 5)) AS NUMERIC(5, 2)) valzonedpos,
+       SUBSTR(flatfld, 16, 5) AS rawzonedneg,
+       INTERPRET(BINARY(SUBSTR(flatfld, 16, 5)) AS NUMERIC(5, 2)) valzonedneg
+  FROM flatfile;       
 
                                                                                       
