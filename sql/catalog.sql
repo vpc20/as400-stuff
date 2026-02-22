2b060312542bf902b14fd64c49adb6c280fd6d81
vpc20 <cuevasvp@gmail.com>
2026-02-22 13:00:30 +0800

    rename files

--- sql/catalog.sql
+++ sql/catalog.sql
@@ -31,6 +31,13 @@ FROM qsys2.syscolumns
 WHERE table_schema = 'VPCRZKH1'
   AND table_name = 'CUSMSTL3';
 
+select table_schema, table_name, column_name, system_column_name, ordinal_position, data_type, length, numeric_scale,
+       system_table_name, system_table_schema
+   from qsys2.syscolumns2
+   where table_schema = 'QSYS2'
+   and table_name = 'SYSPROCS';
+
+
 -- key fields
 SELECT *
   FROM qsys.qadbkatr
