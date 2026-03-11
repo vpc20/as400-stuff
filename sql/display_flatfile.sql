31517f8c77ed97578e091d6b48ad4619a671d53e
vpc20 <cuevasvp@gmail.com>
2026-03-11 16:33:30 +0800

    revision

--- sql/display_flatfile.sql
+++ sql/display_flatfile.sql
@@ -1,15 +1,21 @@
 ﻿set current schema vpcrzkh1;
 
--- display the packed-decimal from the flat file
+__ display the packed_decimal from the flat file
 SELECT SUBSTR(flatfld, 1, 4) AS itemcode, 
        SUBSTR(flatfld, 5, 3) AS rawpackedpos,
-       INTERPRET(BINARY(SUBSTR(flatfld, 5, 3)) AS DECIMAL(5, 2)) valpackedpos,
+       INTERPRET(BINARY(SUBSTR(flatfld, 5, 3)) AS DECIMAL(5, 2)) as valpackedpos,
        SUBSTR(flatfld, 8, 3) AS rawpackedneg,
-       INTERPRET(BINARY(SUBSTR(flatfld, 8, 3)) AS DECIMAL(5, 2)) valpackedneg,
+       INTERPRET(BINARY(SUBSTR(flatfld, 8, 3)) AS DECIMAL(5, 2)) as valpackedneg,
        SUBSTR(flatfld, 11, 5) AS rawzonedpos,
-       INTERPRET(BINARY(SUBSTR(flatfld, 11, 5)) AS NUMERIC(5, 2)) valzonedpos,
+       INTERPRET(BINARY(SUBSTR(flatfld, 11, 5)) AS NUMERIC(5, 2)) as valzonedpos,
        SUBSTR(flatfld, 16, 5) AS rawzonedneg,
-       INTERPRET(BINARY(SUBSTR(flatfld, 16, 5)) AS NUMERIC(5, 2)) valzonedneg
+       INTERPRET(BINARY(SUBSTR(flatfld, 16, 5)) AS NUMERIC(5, 2)) as valzonedneg
   FROM flatfile;       
 
-                                                                                      
+ 
+ SELECT SUBSTR(FLATFLD, 1, 4) AS ITEM_CODE,
+INTERPRET(BINARY(SUBSTR(FLATFLD, 5, 3)) AS DECIMAL(5, 2)) AS VAL_PACKED_POS,
+INTERPRET(BINARY(SUBSTR(FLATFLD, 8, 3)) AS DECIMAL(5, 2)) AS VAL_PACKED_NEG,
+INTERPRET(BINARY(SUBSTR(FLATFLD, 11, 5)) AS NUMERIC(5, 2)) AS VAL_ZONED_POS,
+INTERPRET(BINARY(SUBSTR(FLATFLD, 16, 5)) AS NUMERIC(5, 2)) AS VAL_ZONED_NEG
+FROM FLATFILE;                                                                                   
