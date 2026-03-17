14202dc855f07cea24688b9150f95ae71a0da217
vpc20 <cuevasvp@gmail.com>
2026-03-17 15:24:57 +0800

    revision

--- sql/cross_join.sql
+++ sql/cross_join.sql
@@ -1,7 +1,24 @@
--- Using a Cross Join (No Link)
--- If the tables have no relationship and you just want every possible combination 
+﻿-- Using a Cross Join (No Link)
+-- If the tables have no relationship and you just want every possible combination
 -- (or they both only have one row), you use a CROSS JOIN.
+-- SELECT table1.col1, table2.col2
+-- FROM table1
+-- CROSS JOIN table2;
 
-SELECT table1.col1, table2.col2
-FROM table1
-CROSS JOIN table2;
+with dasoinit as (
+      select job_name,
+             subsystem,
+             authorization_name
+        from table (
+            qsys2.active_job_info(
+              subsystem_list_filter => 'QUSRWRK', job_name_filter => 'QZDASOINIT',
+              current_user_list_filter => 'VPCRZKH')
+          )
+        fetch first 1 rows only)
+  select system_asp_used,
+         job_name,
+         subsystem,
+         authorization_name
+    from qsys2.system_status_info
+         cross join dasoinit;
+
